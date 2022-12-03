90f55a1ce15fc0d7e4888d506f170bcecba3feb7
Adeniyi Bella <adeniyibella@gmail.com>
2022-12-03 13:29:32 +0100

    Updates

---
+++ precache-manifest.6108978704c5ba28b8bc423979b2d01c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "703af7c8ae1af3f2bb27",
+    "url": "/meet-up-app/static/css/main.94278bdd.chunk.css"
+  },
+  {
+    "revision": "703af7c8ae1af3f2bb27",
+    "url": "/meet-up-app/static/js/main.2767a8f6.chunk.js"
+  },
+  {
+    "revision": "f81eb3a11b519627c064",
+    "url": "/meet-up-app/static/js/runtime~main.227e06d2.js"
+  },
+  {
+    "revision": "cb48a3b63a7db4bea2f7",
+    "url": "/meet-up-app/static/js/2.f621d055.chunk.js"
+  },
+  {
+    "revision": "8d1bfcff32ebce11950d",
+    "url": "/meet-up-app/static/js/3.e14308dc.chunk.js"
+  },
+  {
+    "revision": "bc6c1e1e4c3c794a98b835eb8ed68851",
+    "url": "/meet-up-app/index.html"
+  }
+];
