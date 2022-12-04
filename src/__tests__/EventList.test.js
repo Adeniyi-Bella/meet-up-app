47293ce5bdddce7e7e9700c6f18c96fcbd96a27d
Adeniyi Bella <adeniyibella@gmail.com>
2022-12-04 11:21:59 +0100

    implemented pwa and ci

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import EventList from '../EventList';
-import Event from '../Event'; // in src/__tests__/EventList.test.js
+import React from "react";
+import { shallow, mount } from "enzyme";
+import EventList from "../EventList";
+import Event from "../Event";
+import { mockData } from "../mock-data";
+import { getEvents } from "../api";
 
-describe('<EventList /> component', () => {
-  test('render correct number of events', () => {
-    const EventListWrapper = shallow(
-      <EventList
-        events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]}
-      />
-    );
-    expect(EventListWrapper.find(Event)).toHaveLength(4);
+describe("<EventList /> component", () => {
+  test("render correct number of events", () => {
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
+  });
+});
+
+describe("<EventList /> integration", () => {
+  test("render correct number of events", async () => {
+    const allEvents = await getEvents();
+    const EventListWrapper = mount(<EventList events={allEvents} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(allEvents.length);
   });
 });
